b97378d20f3e0701e2addf253753ec518f526ceb
SamueleFacenda <samuele.facenda@gmail.com>
2026-01-27 16:04:45 +0100

    Fix github copilot patch
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- overrides.nix
+++ overrides.nix
@@ -1,4 +1,5 @@
 {
+  nodejs,
   delve,
   stdenv,
   lib,
@@ -28,64 +29,12 @@
     };
   });
   "com.github.copilot" = plugin: plugin.overrideAttrs (old: {
-    # Modified version of https://github.com/ktor/nixos/commit/35f4071faab696b2a4d86643726c9dd3e4293964
+    # This plugins ships with the language server in binary and js form.
+    # The binary form (the default) is very difficult to patch (a patch existed but wasn't stable),
+    # Removing the binaries triggers the fallback option of loading the js language server.
+    propagatedBuildInputs = [ nodejs ];
     buildPhase = ''
-      agent='copilot-agent/native/${lib.toLower stdenv.hostPlatform.uname.system}${
-        {
-          x86_64 = "-x64";
-          aarch64 = "-arm64";
-        }
-        .${stdenv.hostPlatform.uname.processor} or ""
-      }/copilot-language-server'
-      orig_size=$(stat --printf=%s $agent)
-
-      find_payload_offset() {
-        grep -aobUam1 -f <(printf '\x1f\x8b\x08\x00') "$agent" | cut -d: -f1
-      }
-
-      # Helper: find the offset of the prelude by searching for function string start
-      find_prelude_offset() {
-        local prelude_string='(function(process, require, console, EXECPATH_FD, PAYLOAD_POSITION, PAYLOAD_SIZE) {'
-        grep -obUa -- "$prelude_string" "$agent" | cut -d: -f1
-      }
-
-      before_payload_position="$(find_payload_offset)"
-      before_prelude_position="$(find_prelude_offset)"
-
-      patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $agent
-      patchelf --set-rpath ${
-        lib.makeLibraryPath [
-          glibc
-          gcc-unwrapped
-        ]
-      } $agent
-      chmod +x $agent
-      new_size=$(stat --printf=%s $agent)
-      var_skip=20
-      var_select=22
-      shift_by=$(($new_size-$orig_size))
-      function fix_offset {
-        # $1 = name of variable to adjust
-        location=$(grep -obUam1 "$1" $agent | cut -d: -f1)
-        location=$(expr $location + $var_skip)
-        value=$(dd if=$agent iflag=count_bytes,skip_bytes skip=$location \
-        bs=1 count=$var_select status=none)
-        value=$(expr $shift_by + $value)
-        echo -n $value | dd of=$agent bs=1 seek=$location conv=notrunc
-      }
-
-      after_payload_position="$(find_payload_offset)"
-      after_prelude_position="$(find_prelude_offset)"
-
-      if [ "${stdenv.hostPlatform.system}" == "aarch64-linux" ]
-      then
-        fix_offset PAYLOAD_POSITION
-        fix_offset PRELUDE_POSITION
-      else
-        # There are hardcoded positions in the binary, then it replaces the placeholders by himself
-        sed -i -e "s/$before_payload_position/$after_payload_position/g" "$agent"
-        sed -i -e "s/$before_prelude_position/$after_prelude_position/g" "$agent"
-      fi
+      rm -rf copilot-agent/native
     '';
     meta = {
       maintainers = [ "SamueleFacenda" ];
